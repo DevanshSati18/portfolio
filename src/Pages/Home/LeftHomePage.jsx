a006f69f14b84f51f980c9c0f276981eeb83aee9
DevanshSati <whitedevil@kali.devanshsati>
2024-11-12 11:50:27 +0530

    update

--- src/Pages/Home/LeftHomePage.jsx
+++ src/Pages/Home/LeftHomePage.jsx
@@ -1,9 +1,34 @@
-const LeftHomePage =()=>{
-    return(
-        <div>
-            Left Side
+import React from 'react';
+import backgroundImage from '../../Assets/Home'; // Import your background image
+import topImage from '../../Assets/Home'; // Import the round image
+
+const LeftHomePage = () => {
+  return (
+    <div
+      className="relative bg-cover bg-center h-screen p-10"
+      style={{
+        backgroundImage: `url(${backgroundImage})`, // Set the background image
+      }}
+    >
+      <div className="w-full h-full flex justify-center items-start p-10">
+        <div className="bg-white bg-opacity-80 rounded-xl p-10 w-4/5 text-center">
+          {/* Round image on top */}
+          <div className="mt-8 mb-6 px-5">
+            <img
+              src={topImage} // Round image source
+              alt="Round Image"
+              className="w-32 h-32 rounded-full object-cover mx-auto"
+            />
+          </div>
+
+          {/* Your content goes here */}
+          <div>
+            <p>Left Side</p>
+          </div>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
 export default LeftHomePage;
