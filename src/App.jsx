e8b991247fd2f86ba8b4f3de18c12296992f1bfd
devanshsati <devanshsati@devanshsatis-MacBook-Air.local>
2025-05-09 18:11:10 +0530

    allclear

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Resume from "./pages/Resume";
+
+function App() {
+  return (
+    <Router basename="/">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/resume" element={<Resume />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
